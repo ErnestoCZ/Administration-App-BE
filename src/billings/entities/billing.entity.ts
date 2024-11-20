2e4fbcf8d93224413a73bff49ce4d4c50128eb69
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-11-20 23:30:39 +0100

    init Billing Entity

--- src/billings/entities/billing.entity.ts
+++ src/billings/entities/billing.entity.ts
@@ -1,1 +1,20 @@
-export class Billing {}
+import { User } from 'src/users/entities/user.entity';
+import { PrimaryGeneratedColumn, Column, Entity, OneToMany } from 'typeorm';
+
+@Entity()
+export class Billing {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('string')
+  name: string;
+
+  @Column('date')
+  dataFrom: string;
+
+  @Column('date')
+  dataTo: string;
+
+  @OneToMany(() => User, (user) => user.id, { nullable: true })
+  user: User[];
+}
