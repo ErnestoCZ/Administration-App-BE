a372ca657fdbe79569895a43092dc3b5906a23f5
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-11-20 09:07:31 +0100

    changed Entity User id to string instead od number (UUID)

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -3,7 +3,7 @@ import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
-  id: number;
+  id: string;
 
   @Column()
   firstName: string;
