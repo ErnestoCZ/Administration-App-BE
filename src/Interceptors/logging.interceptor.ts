ceac07e4703ff50b9a3fe14acd6059e721752f78
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-12-18 08:54:58 +0100

    added LoggingInterceptor

---
+++ src/Interceptors/logging.interceptor.ts
@@ -0,0 +1,20 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    const response = context.switchToHttp().getResponse();
+    console.log(request.body);
+    console.log(request.headers);
+
+    console.log(response.body);
+    return next.handle();
+  }
+}
