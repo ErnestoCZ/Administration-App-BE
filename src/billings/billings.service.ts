cfcf563a03e1f4778feb4667863618f845426ce9
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-12-20 11:56:36 +0100

    injected UsersService and added addUser() Service

--- src/billings/billings.service.ts
+++ src/billings/billings.service.ts
@@ -1,14 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateBillingDto } from './dto/create-billing.dto';
 import { UpdateBillingDto } from './dto/update-billing.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Billing } from './entities/billing.entity';
 import { Repository } from 'typeorm';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class BillingsService {
   constructor(
     @InjectRepository(Billing) private billingsRepository: Repository<Billing>,
+    @Inject(UsersService) private userService: UsersService,
   ) {}
   async create(createBillingDto: CreateBillingDto) {
     const foundBilling: Billing[] = await this.billingsRepository.find({
@@ -43,5 +45,33 @@ export class BillingsService {
 
   remove(id: number) {
     return `This action removes a #${id} billing`;
+  }
+
+  async addUser(id: string, userId: string) {
+    console.log(id);
+    const foundBilling: Billing = await this.billingsRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
+    if (!foundBilling) {
+      throw new NotFoundException('Billing not found');
+    }
+    console.log(foundBilling);
+
+    const foundUser = await this.userService.findOne(userId);
+    if (!foundUser) {
+      throw new NotFoundException('User not found');
+    }
+
+    const updatedBilling = await foundBilling.user.push(foundUser);
+    console.log(updatedBilling);
+
+    // return foundBilling.user.push(foundUser);
+    return 'This action adds a user to a billing';
+  }
+
+  async removeUser(id: string, userId: string) {
+    return 'This action removes a user from a billing';
   }
 }
