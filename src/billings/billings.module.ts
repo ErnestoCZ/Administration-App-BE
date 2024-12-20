08f860024a7a31a6d6a59e938f9103159a38af96
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-12-20 11:55:09 +0100

    imported UsersModule

--- src/billings/billings.module.ts
+++ src/billings/billings.module.ts
@@ -3,11 +3,12 @@ import { BillingsService } from './billings.service';
 import { BillingsController } from './billings.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Billing } from './entities/billing.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   controllers: [BillingsController],
   providers: [BillingsService, Billing],
-  imports: [TypeOrmModule.forFeature([Billing])],
+  imports: [TypeOrmModule.forFeature([Billing]), UsersModule],
   exports: [Billing],
 })
 export class BillingsModule {}
